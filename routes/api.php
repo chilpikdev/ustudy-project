9c5e508b2be49d119388498305807175f4e96b06
chilpikdev <ihaliev@yandex.ru>
2024-12-04 21:14:00 +0500

    feat: created new posts table

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,6 @@
 
 use App\Http\Controllers\PostController;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Redis;
 use Illuminate\Support\Facades\Route;
 
 Route::get('posts', [PostController::class, 'posts']);
