e4e20660f11fc6e226e53c42ae6c6b5633d07e02
simpledevweb <ibraymovalpamis81@gmail.com>
2025-03-14 11:10:56 +0500

    fix: fixed naming vars

--- app/Actions/Traits/CalculateReadTime.php
+++ app/Actions/Traits/CalculateReadTime.php
@@ -12,26 +12,26 @@ trait CalculateReadTime
     public function calculateTime(string $content): int
     {
         //Clean content from new lines and html tags
-        $cleancontent = strip_tags($content);
-        $cleancontent = str_replace("\n", '', $cleancontent);
-        $cleancontent = array_filter(explode(" ", $cleancontent));
+        $cleanContent = strip_tags($content);
+        $cleanContent = str_replace("\n", '', $cleanContent);
+        $cleanContent = array_filter(explode(" ", $cleanContent));
 
         //Count word from clean content 
-        $wordcount = count($cleancontent);
+        $wordCount = count($cleanContent);
 
         //Count total chars
-        $totalLength = array_sum(array_map('mb_strlen', $cleancontent));
+        $totalLength = array_sum(array_map('mb_strlen', $cleanContent));
 
         //If words is long, wpm will change
-        if ($wordcount != 0) {
+        if ($wordCount != 0) {
             // $wpm = 250; // statistic in the world, average speed word per min
             $wpm = 100;
-            $avgWordLength = $totalLength / $wordcount;
+            $avgWordLength = $totalLength / $wordCount;
             if ($avgWordLength > 6) {
                 // $wpm = 200; // statistic in the world, decrease average wpm  
                 $wpm = 50;
             }
-            return ceil($wordcount / $wpm);
+            return ceil($wordCount / $wpm);
 
         } else {
             return 0;
