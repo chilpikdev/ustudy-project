6aa745d4aeb88ec01226e054e9ff78bc60cc6c0d
bekmurzaevse <bekmurzaevse@gmail.com>
2025-03-14 15:21:56 +0500

    Fixed

--- app/Actions/Admin/v1/Posts/ShareAction.php
+++ app/Actions/Admin/v1/Posts/ShareAction.php
@@ -3,8 +3,10 @@
 namespace App\Actions\Admin\v1\Posts;
 
 use App\Dto\Admin\v1\Posts\ShareDto;
+use App\Exceptions\ApiResponseException;
 use App\Models\Post;
 use App\Traits\ResponseTrait;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Http\JsonResponse;
 
 class ShareAction
@@ -18,11 +20,15 @@ class ShareAction
      */
     public function __invoke(ShareDto $dto): JsonResponse
     {
-        $post = Post::find($dto->postId);
-        $post->increment('shared');
+        try {
+            $post = Post::findOrFail($dto->postId);
+            $post->increment('shared');
 
-        return static::toResponse(
-            message: "Post shared"
-        );
+            return static::toResponse(
+                message: "Post shared"
+            );
+        } catch (ModelNotFoundException $e) {
+            throw new ApiResponseException("Post not found", 404);
+        }
     }
 }
