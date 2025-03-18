8cf40f14ae7aff8c3eba954b1d09ba3a7983cea1
simpledevweb <ibraymovalpamis81@gmail.com>
2025-03-19 00:24:46 +0500

    fix/feat: fixed bugs, added Swagger for PostController(except create,update)

--- app/Http/Controllers/Admin/PostController.php
+++ app/Http/Controllers/Admin/PostController.php
@@ -18,9 +18,35 @@ use App\Http\Requests\Admin\v1\Posts\CreateRequest;
 use App\Http\Requests\Admin\v1\Posts\IndexRequest;
 use App\Http\Requests\Admin\v1\Posts\ShareRequest;
 use Illuminate\Http\JsonResponse;
+use OpenApi\Attributes as OA;
 
 class PostController extends Controller
 {
+    #[OA\Get(
+        path: "/admin/v1/posts",
+        summary: "Get filtered posts",
+        tags: ["Posts Admin"],
+        security: [
+            [
+                'sanctum' => []
+            ]
+        ],
+        parameters: [
+            new OA\Parameter(name: "search", in: "query", description: "nullable|string"),
+            new OA\Parameter(name: "from", in: "query", description: "nullable|date y-m-d"),
+            new OA\Parameter(name: "to", in: "query", description: "nullable|date y-m-d"),
+            new OA\Parameter(name: "order_by", in: "query", description: "nullable|string"),
+            new OA\Parameter(name: "sort", in: "query", description: "nullable|asc, desc"),
+            new OA\Parameter(name: "perpage", in: "query", description: "nullable|int"),
+            new OA\Parameter(name: "page", in: "query", description: "nullable|int")
+        ]
+    )]
+    #[OA\Response(
+        response: 200,
+        description: "Successful Response",
+        content: new OA\JsonContent(ref: "#/components/schemas/AdminIndexRequest")
+    )]
+    #[OA\Response(response: 401, description: 'Unauthenticated')]
     /**
      * Summary of index
      * @param \App\Http\Requests\Admin\v1\Posts\IndexRequest $request
@@ -32,6 +58,23 @@ class PostController extends Controller
         return $action(IndexDto::from($request));
     }
 
+    #[OA\Post(
+        path: '/admin/v1/posts/create',
+        summary: "Create Post",
+        description: "Not done",
+        tags: ["Posts Admin"],
+        security: [
+            [
+                'sanctum' => []
+            ]
+        ]
+    )]
+    /**Request Body ----*/
+    #[OA\Response(
+        response: 200,
+        description: "Post Created",
+    )]
+    #[OA\Response(response: 401, description: 'Unauthenticated')]
     /**
      * Summary of create
      * @param \App\Http\Requests\Admin\v1\Posts\CreateRequest $request
@@ -43,6 +86,24 @@ class PostController extends Controller
         return $action(CreateDto::from($request));
     }
 
+    #[OA\Get(
+        path: "/admin/v1/posts/show/{id}",
+        summary: "Get Post Details",
+        tags: ["Posts Admin"],
+        security: [
+            ["sanctum" => []]
+        ],
+    )]
+    #[OA\Parameter(
+        name: "id",
+        description: "Post ID",
+        in: "path",
+        required: true,
+        schema: new OA\Schema(type: "integer", example: 1)
+    )]
+    #[OA\Response(response: 200, description: "Post details")]
+    #[OA\Response(response: 404, description: "Post not found")]
+    #[OA\Response(response: 401, description: "Unauthenticated")]
     /**
      * Summary of show
      * @param int $id
@@ -54,6 +115,23 @@ class PostController extends Controller
         return $action($id);
     }
 
+    #[OA\Put(
+        path: '/admin/v1/posts/update',
+        description: "Not done",
+        summary: "Create Post",
+        tags: ["Posts Admin"],
+        security: [
+            [
+                'sanctum' => []
+            ]
+        ]
+    )]
+    /**Request Body ----*/
+    #[OA\Response(
+        response: 200,
+        description: "Post Updated",
+    )]
+    #[OA\Response(response: 401, description: 'Unauthenticated')]
     /**
      * Summary of update
      * @param \App\Http\Requests\Admin\v1\Posts\UpdateRequest $request
@@ -65,6 +143,24 @@ class PostController extends Controller
         return $action(UpdateDto::from($request));
     }
 
+    #[OA\Delete(
+        path: "/admin/v1/posts/delete/{id}",
+        summary: "Delete Post Details",
+        tags: ["Posts Admin"],
+        security: [
+            ["sanctum" => []]
+        ],
+    )]
+    #[OA\Parameter(
+        name: "id",
+        description: "Post ID",
+        in: "path",
+        required: true,
+        schema: new OA\Schema(type: "integer", example: 1)
+    )]
+    #[OA\Response(response: 200, description: "Post deleted")]
+    #[OA\Response(response: 404, description: "Post not found")]
+    #[OA\Response(response: 401, description: "Unauthenticated")]
     /**
      * Summary of delete
      * @param int $id
@@ -76,6 +172,36 @@ class PostController extends Controller
         return $action($id);
     }
 
+    #[OA\Post(
+        path: "/admin/v1/posts/share",
+        summary: "Share Post",
+        tags: ["Posts Admin"],
+        security: [
+            ["sanctum" => []]
+        ],
+    )]
+    #[OA\RequestBody(
+        required: true,
+        content: new OA\MediaType(
+            mediaType: "application/json",
+            schema: new OA\Schema(
+                type: "object",
+                required: ["post_id"],
+                properties: [
+                    new OA\Property(property: "post_id", type: "integer", example: 1)
+                ]
+            )
+        )
+    )]
+    #[OA\Response(response: 200, description: "Post shared")]
+    #[OA\Response(response: 404, description: "Post not found")]
+    #[OA\Response(response: 401, description: "Unauthenticated")]
+    /**
+     * Summary of share
+     * @param \App\Http\Requests\Admin\v1\Posts\ShareRequest $request
+     * @param \App\Actions\Admin\v1\Posts\ShareAction $action
+     * @return JsonResponse
+     */
     public function share(ShareRequest $request, ShareAction $action)
     {
         return $action(ShareDto::from($request));
--- app/Http/Controllers/Core/PostController.php
+++ app/Http/Controllers/Core/PostController.php
@@ -9,9 +9,14 @@ use App\Dto\Core\v1\Posts\IndexDto;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Core\v1\Posts\IndexRequest;
 use Illuminate\Http\JsonResponse;
+use OpenApi\Attributes as OA;
+
 
 class PostController extends Controller
 {
+    #[OA\Get(path: '/core/v1/posts', tags: ["Posts Core"], summary: "Retrieve all posts", )]
+    #[OA\Response(response: 200, description: 'Post collection with pagination')]
+    #[OA\Response(response: 401, description: 'Unauthenticated')]
     /**
      * Summary of posts
      */
@@ -20,6 +25,16 @@ class PostController extends Controller
         return $action(IndexDto::from($request));
     }
 
+    #[OA\Get(path: '/core/v1/posts/show/{slug}', summary: "Post by slug detail", tags: ["Posts Core"])]
+    #[OA\Parameter(
+        name: "slug",
+        in: "path",
+        required: true,
+        description: "The slug of the post",
+        schema: new OA\Schema(type: "string", example: "my-first-post")
+    )]
+    #[OA\Response(response: 200, description: 'Post by slug')]
+    #[OA\Response(response: 401, description: 'Unauthenticated')]
     /**
      * Summary of show
      */
@@ -28,6 +43,9 @@ class PostController extends Controller
         return $action($slug);
     }
 
+    #[OA\Get(path: '/core/v1/posts/recommended', summary: "5 Latest recommended post", tags: ["Posts Core"])]
+    #[OA\Response(response: 200, description: '5 Latest recommended post')]
+    #[OA\Response(response: 401, description: 'Unauthenticated')]
     /**
      * Summary of recommended
      * @param \App\Actions\Core\v1\Posts\RecommendedAction $action
